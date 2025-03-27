22f24a01d87e73c4857b4aed681ff79df0020b3b
ysmreg <33682244+ysmreg@users.noreply.github.com>
2025-03-27 19:59:19 +0900

    change data dir for rincoin

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -655,7 +655,7 @@ fs::path GetDefaultDataDir()
     // Unix-like: ~/.bitcoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Litecoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Rincoin";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -665,10 +665,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // macOS
-    return pathRet / "Library/Application Support/Litecoin";
+    return pathRet / "Library/Application Support/Rincoin";
 #else
     // Unix-like
-    return pathRet / ".litecoin";
+    return pathRet / ".rincoin";
 #endif
 #endif
 }
