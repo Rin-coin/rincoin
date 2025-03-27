930d9e327dd3c0e0b9882252c0383f2b5e315c4e
ysmreg <33682244+ysmreg@users.noreply.github.com>
2025-03-27 22:41:43 +0900

    Update boost.mk

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -34,6 +34,7 @@ endef
 
 define $(package)_preprocess_cmds
   patch -p1 < $($(package)_patch_dir)/unused_var_in_process.patch && \
+  patch -p1 < $($(package)_patch_dir)/fix-pthread-stack.patch && \
   echo "using $($(package)_toolset_$(host_os)) : : $($(package)_cxx) : <cxxflags>\"$($(package)_cxxflags) $($(package)_cppflags)\" <linkflags>\"$($(package)_ldflags)\" <archiver>\"$($(package)_archiver_$(host_os))\" <striper>\"$(host_STRIP)\"  <ranlib>\"$(host_RANLIB)\" <rc>\"$(host_WINDRES)\" : ;" > user-config.jam
 endef
 
